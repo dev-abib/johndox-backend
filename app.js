a4ec5d46c4988686bc5205555b5e4808e5023e47
sultan93291 <abibdipto@gmail.com>
2026-01-08 16:07:27 +0600

    feat:live url added to fix cors issue

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://johndox-admin-dashboard-31uw.vercel.app/",
+      "https://johndox-admin-dashboard-31uw.vercel.app",
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     credentials: true,
