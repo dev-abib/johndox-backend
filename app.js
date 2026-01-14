0a860c7b49e1ba8cd4ea3364ebdd170868835341
sultan93291 <abibdipto@gmail.com>
2026-01-14 12:12:59 +0600

    feat:cors updated

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://johndox-admin-dashboard.vercel.app/",
+      "https://johndox-admin-dashboard.vercel.app",
       "http://localhost:3000",
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
