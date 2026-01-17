d70ba6e973e6fc8ed31fbf06d489d5b4b0f261e5
sultan93291 <abibdipto@gmail.com>
2026-01-17 14:16:41 +0600

    feat: property controller location error fixed

--- src/Controller/property.controller.js
+++ src/Controller/property.controller.js
@@ -383,7 +383,80 @@ const updateProperty = asyncHandler(async (req, res, next) => {
   }
 
   const addressString = `${fullAddress}, ${city}, ${state}`;
-  const { lat, lng } = await geocodeAddress(addressString);
+  if (!addressString.trim()) {
+    return next(new apiError(400, "Invalid address provided", null, false));
+  }
+
+  const key = process.env.LOCATIONIQ_KEY;
+  if (!key) return next(new apiError(500, "No Location key provided"));
+
+  const url = "https://us1.locationiq.com/v1/search";
+
+  const response = await axios
+    .get(url, {
+      params: {
+        key,
+        q: fullAddress,
+        format: "json",
+        limit: 1,
+        addressdetails: 1,
+        normalizecity: 1,
+      },
+      timeout: 10000,
+    })
+    .catch((error) => {
+      if (error.response) {
+        return next(
+          new apiError(
+            error.response.status,
+            `${error.response.data.error} address` ||
+              "Geocoding request failed.",
+            null,
+            false
+          )
+        );
+      } else if (error.request) {
+        return next(
+          new apiError(
+            500,
+            "Network error or no response from the geocoding service.",
+            null,
+            false
+          )
+        );
+      } else {
+        return next(
+          new apiError(
+            500,
+            error.message || "An unknown error occurred during geocoding.",
+            null,
+            false
+          )
+        );
+      }
+    });
+
+  if (!response?.data || response.data.length === 0) {
+    return next(
+      new apiError(400, "Unable to geocode the provided address", null, false)
+    );
+  }
+
+  const lat = Number(response.data[0].lat);
+  const lng = Number(response.data[0].lon);
+
+  // Validate lat and lng
+  if (!lat || !lng) {
+    return next(new apiError(401, "Address not found."));
+  }
+
+  if (!lat || !lng) {
+    return next(
+      new apiError(400, "Unable to geocode the provided address", null, false)
+    );
+  }
+
+
 
   const files = req.files || {};
   const photoFiles = files.photos || [];
